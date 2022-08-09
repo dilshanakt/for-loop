3f41761043800fb90269fa428ae70377329cea17
dilshanakt <110896961+dilshanakt@users.noreply.github.com>
2022-08-09 15:25:28 +0530

    first commit

---
+++ dil.py
@@ -0,0 +1,10 @@
+b="Dilshana"
+print(b.upper())
+print(b.lower())
+print(b.capitalize())
+name="  i am dilshana  "
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+var="i am dilshana"
+print(var.replace("i am","my name is"))
