a0595bced1bbd080b0c9fa6b64faff896fdc58b3
dilshanakt <110896961+dilshanakt@users.noreply.github.com>
2022-08-09 15:31:01 +0530

    first commit

---
+++ identifiers.py
@@ -0,0 +1,23 @@
+var="dilsha"
+print(var.isidentifier())
+var="dilsha13"
+print(var.isidentifier())
+var="dilsha#"
+print(var.isidentifier())
+var="22dilsha"
+print(var.isidentifier())
+print("_____________________")
+var="dilsha dilsha"
+print(var.isspace())
+var="     "
+print(var.isspace())
+var_title="I am The best student"
+print(var_title.istitle())
+var_title="I Am The Best Student"
+print(var_title.istitle())
+is_upper="I AM THE BEST STUDENT"
+print(is_upper.isupper())
+is_lower="i am the best student"
+print(is_lower.islower())
+is_upper="i AM THE BEST STUDEN"
+print(is_upper.isupper())
