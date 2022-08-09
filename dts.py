064d5fabf80f641e9e6ba1794926940d39617a5d
dilshanakt <110896961+dilshanakt@users.noreply.github.com>
2022-08-09 15:26:00 +0530

    first commit

---
+++ dts.py
@@ -0,0 +1,20 @@
+
+a=3
+print(type(a))
+multiline='''this is a beautiful place.
+iam in calicut.
+my friends are very pretty girls.'''
+print(multiline)
+print(len(multiline))
+print("akku dil"in multiline)
+str="one two three four five six seven eight nine ten"
+print(str[0:3])
+print(str[4:7])
+print(str[8:13])
+print(str[14:19])
+print(str[19:24])
+print(str[24:28])
+print(str[28:34])
+print(str[34:40])
+print(str[40:45])
+print(str[45:49])
