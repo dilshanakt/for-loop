c3506ef0a3db9aefcd8ab728945e283c5a9156c9
dilshanakt <110896961+dilshanakt@users.noreply.github.com>
2022-08-09 15:16:48 +0530

    first commit

---
+++ nested_dic.py
@@ -0,0 +1,22 @@
+nested_dictionary={
+    "one":
+        {
+            "name":"john",
+            "age":30,
+            "city":"new york"
+        },
+    "two":
+        {
+            "name":"jane",
+            "age":25,
+            "city":"paris"
+        },
+    "three":
+        {
+            "name":"boby",
+            "age":20,
+            "city":"london"
+ }
+}
+print(nested_dictionary)
+print(nested_dictionary["three"]["name"])
